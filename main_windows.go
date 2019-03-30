8bf7d582224b1a6eae60840fb3bb876a2d276205
bm201894 <milesboz@me.com>
2019-03-30 13:35:37 -0500

    changed stuff

--- main_windows.go
+++ main_windows.go
@@ -1,11 +1,16 @@
+//+build windows
 package main
 
 import "os/exec"
 
-func GetCommand() []string {
+func GetCommand() string {
 	return "reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v Wallpaper /t REG_SZ /d  wallpaper_path /f"
 }
 
 func Update() {
-	cmd, err := exec.Command("RUNDLL32.EXE", "user32.dll,UpdatePerUserSystemParameters")
+	cmd := exec.Command("RUNDLL32.EXE", "user32.dll,UpdatePerUserSystemParameters")
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
 }
